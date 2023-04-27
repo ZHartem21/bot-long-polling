3cadae2047c600790ba66217499700dc8c6f0610
ZHartem21 <zhartem21@gmail.com>
2023-04-27 20:09:10 +0300

    Fix timestamp and replace some indices

--- main.py
+++ main.py
@@ -17,29 +17,32 @@ def main():
     tg_chat_id = os.environ['TG_CHAT_ID']
     bot = telegram.Bot(token=tg_token)
     headers = {'Authorization': 'Token {0}'.format(dvmn_token)}
+    timestamp = None
     while True:
         try:
             if timestamp:
-                params = {'Timestamp': timestamp}
-            response = requests.get(USER_REVIEWS_LONG_POLLING, headers=headers, timeout=90, params=params)
+                response = requests.get(USER_REVIEWS_LONG_POLLING, headers=headers, timeout=90, params={'Timestamp': timestamp})
+            else:
+                response = requests.get(USER_REVIEWS_LONG_POLLING, headers=headers, timeout=90,)
             response.raise_for_status()
         except requests.exceptions.ReadTimeout or ConnectionError:
             sleep(30)
             continue
         review = response.json()
-        if review['new_attempts'][0]['is_negative'] and review['status'] = 'found':
+        last_review = review['new_attempts'][0]
+        timestamp = last_review['timestamp']
+        if last_review['is_negative'] and review['status'] == 'found':
             message_text = dedent(f'''\
-                У вас проверили работу "{review['new_attempts'][0]['lesson_title']}".\n
+                У вас проверили работу "{last_review['lesson_title']}".\n
                 К сожалению, в работе нашлись ошибки.\n
-                Ссылка на урок: {review['new_attempts'][0]['lesson_url']}''')
+                Ссылка на урок: {last_review['lesson_url']}''')
             bot.send_message(text=message_text, chat_id=tg_chat_id)
-        if not review['new_attempts'][0]['is_negative']:
+        if not last_review['is_negative']:
             message_text = dedent(f'''\
-                У вас проверили работу "{review['new_attempts'][0]['lesson_title']}".\n
+                У вас проверили работу "{last_review['lesson_title']}".\n
                 Преподавателю всё понравилось, можно приступать к следующему уроку!\n
-                Ссылка на урок: {review['new_attempts'][0]['lesson_url']}''')
+                Ссылка на урок: {last_review['lesson_url']}''')
             bot.send_message(text=message_text, chat_id=tg_chat_id)
-        timestamp = review['new_attempts'][0]['timestamp']
 
 
 if __name__ == '__main__':
