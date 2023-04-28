3ca16bd9e7ca321812d60faab059188c288afbbf
ZHartem21 <zhartem21@gmail.com>
2023-04-28 21:46:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,7 @@ def main():
     timestamp = None
     while True:
         try:
-            if timestamp:
-                response = requests.get(USER_REVIEWS_LONG_POLLING, headers=headers, timeout=90, params={'Timestamp': timestamp})
-            else:
-                response = requests.get(USER_REVIEWS_LONG_POLLING, headers=headers, timeout=90,)
+            response = requests.get(USER_REVIEWS_LONG_POLLING, headers=headers, timeout=105, params={'Timestamp': timestamp})
             response.raise_for_status()
         except requests.exceptions.ReadTimeout:
             continue
@@ -31,20 +28,23 @@ def main():
             sleep(30)
             continue
         review = response.json()
-        last_review = review['new_attempts'][0]
-        timestamp = last_review['timestamp']
-        if last_review['is_negative'] and review['status'] == 'found':
-            message_text = dedent(f'''\
-                У вас проверили работу "{last_review['lesson_title']}".\n
-                К сожалению, в работе нашлись ошибки.\n
-                Ссылка на урок: {last_review['lesson_url']}''')
-            bot.send_message(text=message_text, chat_id=tg_chat_id)
-        if not last_review['is_negative']:
-            message_text = dedent(f'''\
-                У вас проверили работу "{last_review['lesson_title']}".\n
-                Преподавателю всё понравилось, можно приступать к следующему уроку!\n
-                Ссылка на урок: {last_review['lesson_url']}''')
-            bot.send_message(text=message_text, chat_id=tg_chat_id)
+        if review['status'] == 'found':
+            if review['new_attempts'][0]['is_negative']:
+                last_review = review['new_attempts'][0]
+                timestamp = last_review['timestamp']
+                message_text = dedent(f'''\
+                    У вас проверили работу "{last_review['lesson_title']}".\n
+                    К сожалению, в работе нашлись ошибки.\n
+                    Ссылка на урок: {last_review['lesson_url']}''')
+                bot.send_message(text=message_text, chat_id=tg_chat_id)
+            if not last_review['is_negative']:
+                message_text = dedent(f'''\
+                    У вас проверили работу "{last_review['lesson_title']}".\n
+                    Преподавателю всё понравилось, можно приступать к следующему уроку!\n
+                    Ссылка на урок: {last_review['lesson_url']}''')
+                bot.send_message(text=message_text, chat_id=tg_chat_id)
+        else:
+            continue
 
 
 if __name__ == '__main__':
