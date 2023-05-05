af3fd00923ce0b830dfc46a64f2cfa5cf6044585
ZHartem21 <zhartem21@gmail.com>
2023-05-05 17:31:40 +0300

    Catch errors and write to log

--- main.py
+++ main.py
@@ -23,50 +23,54 @@ class TelegramHandler(logging.Handler):
 
 
 def main():
-    load_dotenv('.env')
-    dvmn_token = os.environ['DVMN_TOKEN']
-    tg_token = os.environ['TG_TOKEN']
-    tg_chat_id = os.environ['TG_CHAT_ID']
-    bot = telegram.Bot(token=tg_token)
-    headers = {'Authorization': 'Token {0}'.format(dvmn_token)}
-    timestamp = None
-    logging.basicConfig(format="%(message)s", level=logging.INFO)
-    logger = logging.getLogger('Logger')
-    logger.setLevel(logging.INFO)
-    logger.addHandler(TelegramHandler(bot, tg_chat_id))
-    logger.info('Bot started')
-    while True:
-        try:
-            response = requests.get(USER_REVIEWS_LONG_POLLING, headers=headers, timeout=90, params={'Timestamp': timestamp})
-            response.raise_for_status()
-        except requests.exceptions.ReadTimeout:
-            logger.debug('Read timeout on request')
-            continue
-        except ConnectionError:
-            logger.warning('Connection error on request')
-            sleep(30)
-            continue
-        review = response.json()
-        if review['status'] == 'found':
-            if review['new_attempts'][0]['is_negative']:
-                last_review = review['new_attempts'][0]
-                timestamp = last_review['timestamp']
-                message_text = dedent(f'''\
-                    У вас проверили работу "{last_review['lesson_title']}".\n
-                    К сожалению, в работе нашлись ошибки.\n
-                    Ссылка на урок: {last_review['lesson_url']}''')
-                bot.send_message(text=message_text, chat_id=tg_chat_id)
-            if not last_review['is_negative']:
-                message_text = dedent(f'''\
-                    У вас проверили работу "{last_review['lesson_title']}".\n
-                    Преподавателю всё понравилось, можно приступать к следующему уроку!\n
-                    Ссылка на урок: {last_review['lesson_url']}''')
-                bot.send_message(text=message_text, chat_id=tg_chat_id)
-            logger.info('Bot has processed review')
+    try:
+        load_dotenv('.env')
+        dvmn_token = os.environ['DVMN_TOKEN']
+        tg_token = os.environ['TG_TOKEN']
+        tg_chat_id = os.environ['TG_CHAT_ID']
+        bot = telegram.Bot(token=tg_token)
+        headers = {'Authorization': 'Token {0}'.format(dvmn_token)}
+        timestamp = None
+        logging.basicConfig(format="%(message)s", level=logging.INFO)
+        logger = logging.getLogger('Logger')
+        logger.setLevel(logging.INFO)
+        logger.addHandler(TelegramHandler(bot, tg_chat_id))
+        logger.info('Bot started')
+        while True:
+            try:
+                response = requests.get(USER_REVIEWS_LONG_POLLING, headers=headers, timeout=90, params={'Timestamp': timestamp})
+                response.raise_for_status()
+            except requests.exceptions.ReadTimeout:
+                logger.debug('Read timeout on request')
+                continue
+            except ConnectionError:
+                logger.warning('Connection error on request')
+                sleep(30)
+                continue
+            review = response.json()
+            if review['status'] == 'found':
+                if review['new_attempts'][0]['is_negative']:
+                    last_review = review['new_attempts'][0]
+                    timestamp = last_review['timestamp']
+                    message_text = dedent(f'''\
+                        У вас проверили работу "{last_review['lesson_title']}".\n
+                        К сожалению, в работе нашлись ошибки.\n
+                        Ссылка на урок: {last_review['lesson_url']}''')
+                    bot.send_message(text=message_text, chat_id=tg_chat_id)
+                if not last_review['is_negative']:
+                    message_text = dedent(f'''\
+                        У вас проверили работу "{last_review['lesson_title']}".\n
+                        Преподавателю всё понравилось, можно приступать к следующему уроку!\n
+                        Ссылка на урок: {last_review['lesson_url']}''')
+                    bot.send_message(text=message_text, chat_id=tg_chat_id)
+                logger.info('Bot has processed review')
 
-        else:
-            continue
+            else:
+                continue
 
+    except Exception as err:
+        logger.error('Бот упал с ошибкой:')
+        logger.exception(err)
 
 if __name__ == '__main__':
     main()
